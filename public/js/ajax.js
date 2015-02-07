2de81bf59d024e6b7696814270348269c45e4dbf
Sangha <sanghal@andrew.cmu.edu>
2015-02-07 14:41:06 -0500

    commited

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -44,7 +44,7 @@ function getUserTweetOnDelay(id) {
     	$(".flip-container").toggleClass('buttonclicked');
     	$("div#front").empty();
 		$("div#back").empty();
-    	getUserTweets(id); }, 1000);
+    	getUserTweets(id); }, 2000);
 }
 
 function insertChoices() {
@@ -80,7 +80,7 @@ function insertChoices() {
 
 			$("div#theSCORE").append("<div id=\"theEND\" class=\"slideUp\"></div>");
 			$("div#theEND").append("<button id=\"refresh\"> Play again </button>");
-			$("div#theEND").append("<a class=\"twitter-share-button\"href=\"https://twitter.com/share\" data-text=\"I received " +score+ " points for TwitterQuizzer. How well do you know your friend? \"data-via=\"twitterdev\">Tweet</a><script>window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src=\"https://platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,\"script\",\"twitter-wjs\"));</script>");
+			$("div#theEND").append("<a class=\"twitter-share-button\"href=\"https://twitter.com/share\" data-text=\"I received " +score+ " point(s) for TwitterQuizzer. How well do you know your friend? \"data-via=\"twitterdev\">Tweet</a><script>window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src=\"https://platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,\"script\",\"twitter-wjs\"));</script>");
 			$("#refresh").click(refresh);
 		}
 
